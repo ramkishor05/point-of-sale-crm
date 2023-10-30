f94e4d8e71e0680d6a7b3d1b2158dd4bb27f79bd
Ram Kishor <ram@omniesolutions.com>
2023-10-30 20:42:08 +0530

    ok

--- target/generated-sources/annotations/com/brijframework/crm/mapper/impl/VendorDetailMapperImpl.java
+++ target/generated-sources/annotations/com/brijframework/crm/mapper/impl/VendorDetailMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-30T01:15:40+0530",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.19 (Oracle Corporation)"
+    date = "2023-10-30T13:39:15+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_202 (Oracle Corporation)"
 )
 @Component
 public class VendorDetailMapperImpl implements VendorDetailMapper {
